e780958fbdc8b6186054fae7633692faafeec4cf
vhemeret <val.kawa@icloud.com>
2022-07-23 16:25:12 +0200

    all utils was normed

--- srcs/utils/mem.c
+++ srcs/utils/mem.c
@@ -6,77 +6,77 @@
 /*   By: vahemere <vahemere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 00:05:59 by vahemere          #+#    #+#             */
-/*   Updated: 2022/07/23 00:25:48 by vahemere         ###   ########.fr       */
+/*   Updated: 2022/07/23 16:24:28 by vahemere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-void    add_mem(void *addr)
+void	add_mem(void *addr)
 {
-    t_mem   *new_mem;
-    t_mem   *tmp;
+	t_mem	*new_mem;
+	t_mem	*tmp;
 
-    new_mem = malloc(sizeof(t_mem));
-    new_mem->addr = addr;
-    new_mem->next = NULL;
-    if (all.g_mem == NULL)
-        all.g_mem = new_mem;
-    else
-    {
-        tmp = all.g_mem;
-        while (tmp->next)
-            tmp = tmp->next;
-        tmp->next = new_mem;
-    }
+	new_mem = malloc(sizeof(t_mem));
+	new_mem->addr = addr;
+	new_mem->next = NULL;
+	if (all.g_mem == NULL)
+		all.g_mem = new_mem;
+	else
+	{
+		tmp = all.g_mem;
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new_mem;
+	}
 }
 
-void    *ft_malloc(size_t size)
+void	*ft_malloc(size_t size)
 {
-    void    *addr;
+	void	*addr;
 
-    addr = malloc(size);
-    add_mem(addr);
-    return (addr);
+	addr = malloc(size);
+	add_mem(addr);
+	return (addr);
 }
 
-void    ft_free(void *addr)
+void	ft_free(void *addr)
 {
-    t_mem   *tmp;
-    t_mem   *ex;
-    t_mem   *next;
+	t_mem	*tmp;
+	t_mem	*ex;
+	t_mem	*next;
 
-    tmp = all.g_mem;
-    ex = NULL;
-    while (tmp)
-    {
-        next = tmp->next;
-        if (tmp->addr == addr)
-        {
-            free(tmp->addr);
-            free(tmp);
-            if (ex)
-                ex->next = next;
-            else
-                all.g_mem = next;
-            return ;
-        }
-        ex = tmp;
-        tmp = tmp->next;
-    }
+	tmp = all.g_mem;
+	ex = NULL;
+	while (tmp)
+	{
+		next = tmp->next;
+		if (tmp->addr == addr)
+		{
+			free(tmp->addr);
+			free(tmp);
+			if (ex)
+				ex->next = next;
+			else
+				all.g_mem = next;
+			return ;
+		}
+		ex = tmp;
+		tmp = tmp->next;
+	}
 }
 
-void    ft_free_all()
+void	ft_free_all(void)
 {
-    t_mem   *tmp;
-    t_mem   *next;
+	t_mem	*tmp;
+	t_mem	*next;
 
-    tmp = all.g_mem;
-    while (tmp)
-    {
-        next = tmp->next;
-        free(tmp->addr);
-        free(tmp);
-        tmp = next;
-    }
+	tmp = all.g_mem;
+	while (tmp)
+	{
+		next = tmp->next;
+		free(tmp->addr);
+		free(tmp);
+		tmp = next;
+	}
 }
